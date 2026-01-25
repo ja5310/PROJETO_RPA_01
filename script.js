147407dd1134c86e436965bda3262c9a2b8b8bca
seu-ja5310 <joseantoniosilva763@gmail.com>
2026-01-25 11:47:58 -0300

    Inclusão de Js e animação

---
+++ script.js
@@ -0,0 +1,50 @@
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu-mobile");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+
+/* ======================
+   CONTADOR
+====================== */
+const counters = document.querySelectorAll(".counter");
+
+const startCounter = (counter) => {
+  const target = +counter.getAttribute("data-target");
+  const prefix = counter.getAttribute("data-prefix") || "";
+  let current = 0;
+  const increment = target / 100; // velocidade
+
+  const updateCounter = () => {
+    current += increment;
+
+    if (current < target) {
+      const value = target % 1 === 0 ? Math.ceil(current) : current.toFixed(1);
+
+      counter.innerText = `${prefix}${value}`;
+      requestAnimationFrame(updateCounter);
+    } else {
+      counter.innerText = `${prefix}${target}`;
+    }
+  };
+
+  updateCounter();
+};
+
+/* ======================
+   QUANDO ENTRA NA TELA
+====================== */
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        startCounter(entry.target);
+        observer.unobserve(entry.target); // anima só uma vez
+      }
+    });
+  },
+  { threshold: 0.5 },
+);
+
+counters.forEach((counter) => observer.observe(counter));
